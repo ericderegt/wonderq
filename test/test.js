0412e835a34769eb8fcc44d80e74c331863354f6
Eric deRegt <ericderegt@gmail.com>
2018-10-09 08:59:52 -0400

    first test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('WonderQ', function() {
+  describe('first test', function() {
+    it('should return value', function() {
+      assert.equal([1,2,3].indexOf(2), 1);
+    });
+  });
+});
