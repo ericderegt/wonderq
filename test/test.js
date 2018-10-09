e061827b3d4e35696a149eb730cdf65f2015d8ac
Eric deRegt <ericderegt@gmail.com>
2018-10-09 10:02:15 -0400

    add tests for basic queue create, add, poll

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
 var assert = require('assert');
 
 describe('WonderQ', function() {
-  describe('first test', function() {
-    it('should return value', function() {
-      assert.equal([1,2,3].indexOf(2), 1);
-    });
+  it('Create a queue', function() {
+    const queue = WonderQ('Queue');
+    // assert.equal([1,2,3].indexOf(2), 1);
+    (queue instanceof WonderQ).should.be.true;
+  });
+
+  it('Write message should return ID', function() {
+    const queue = WonderQ('Queue');
+    const message = {'timestamp': new Date(), 'body': 'this is the body'};
+
+    const msg = queue.writeMessage(message);
+    expect(msg).to.be.Number();
   });
 });
